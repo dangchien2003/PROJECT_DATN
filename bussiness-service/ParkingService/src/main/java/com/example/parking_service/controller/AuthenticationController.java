6a8ca1eeca0f062843b61bb4d33f268f46df4a9a
chienld <chienboy03@gmail.com>
2025-04-01 01:05:07 +0700

    cấu hình cors cho parking service

--- bussiness-service/ParkingService/src/main/java/com/example/parking_service/controller/AuthenticationController.java
+++ bussiness-service/ParkingService/src/main/java/com/example/parking_service/controller/AuthenticationController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +20,14 @@ public class AuthenticationController {
     ApiResponse<Object> createAccount(@Valid @RequestBody AccountRequest request) {
         return authenticationService.createAccount(request, null);
     }
+
+    @GetMapping("/get")
+    ApiResponse<Object> getTesst() {
+        return ApiResponse.builder()
+                .result("abc")
+                .build();
+    }
+
 
     @PostMapping("/create/admin")
     ApiResponse<Object> createAccountByAdmin(@Valid @RequestBody AccountRequest request) {
