af07e23a384f9ba1bf0d9d836e89150929553fab
chienld <chienboy03@gmail.com>
2025-07-12 21:28:02 +0700

    Update webpack.config.js

--- front-end-smart-parking/webpack.config.js
+++ front-end-smart-parking/webpack.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   module: {
     rules: [
@@ -5,6 +7,18 @@ module.exports = {
         test: /\.svg$/,
         use: ["@svgr/webpack"],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "static/media/[name].[hash][ext]",
+        },
+      },
     ],
+  },
+  resolve: {
+    alias: {
+      "@image": path.resolve(__dirname, "src/assets/image"),
+    },
   },
 };
